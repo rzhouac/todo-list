c75d307ed23d3dfd060f42445d58b8fa937c46c8
rzhouac <rzhouac@connect.ust.hk>
2018-07-16 22:33:24 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  newTodo = '';
+  todoList: Array<any> = [];
+
+  onAddTodo() {
+    this.todoList.push({name: this.newTodo, finished: false});
+    this.newTodo = '';
+  }
+
+  finishItem(index) {
+    this.todoList[index].finished = true;
+  }
 }
